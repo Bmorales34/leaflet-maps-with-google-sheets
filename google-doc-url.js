69c0872e642842ace1f9c3e860fb2b0e944ac648
Bmorales34 <131725336+Bmorales34@users.noreply.github.com>
2025-07-08 16:03:18 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,2 @@
 // data stored in csv folder on GitHub, not Google Sheets
 var googleDocURL = null;
-
-// insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
